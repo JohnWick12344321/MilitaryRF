c12fd9bbf16a43b6432b195343f00e1abee85527
JohnWick12344321 <Kondit1111@mail.ru>
2022-12-02 17:40:57 +0300

    в

--- src/main/java/org/example/soldierChecker.java
+++ src/main/java/org/example/soldierChecker.java
@@ -3,18 +3,18 @@ package org.example;
 public class soldierChecker {
     public boolean check(Soldier soldier) {
         if (soldier.age > 18 && soldier.age < 30 && soldier.Children < 3 && soldier.category == 'A' && (soldier.speciality == Speciality.Svyazist || soldier.speciality == Speciality.SNIPER || soldier.speciality == Speciality.TANKIST)) {
-            System.out.println("Первый эшелон");
+            System.out.println("Вы попадаете в первый эшелон!");
             return true;
 
         } else if (soldier.age > 18 && soldier.age <= 45 && (soldier.Children == 3 || soldier.Children < 4 && soldier.category == 'B' || soldier.category == 'A' && soldier.speciality == Speciality.TEXNIK || soldier.speciality == Speciality.Voditel)) {
-            System.out.println("Второй эшелон");
+            System.out.println("Вы попадаете во второй эешелон!");
             return true;
 
         } else if (soldier.age >= 60 || soldier.category == 'D') {
-            System.out.println("Вы не подходите");
+            System.out.println("Вы не подходите!");
             return true;
         } else {
-            System.out.println("Третий эшелон");
+            System.out.println("Вы попадаете в третий эшелон!");
             return false;
         }
     }
